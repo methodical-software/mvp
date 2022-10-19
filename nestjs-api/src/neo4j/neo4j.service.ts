2cc4eb7cab7d93f27c5c1b02f0d768874e6e7082
Konstantin <dev.shevchenko.konstantin@gmail.com>
2022-10-19 18:44:13 +0300

    fix response to return data in format of basicprimitives

--- nestjs-api/src/neo4j/neo4j.service.ts
+++ nestjs-api/src/neo4j/neo4j.service.ts
@@ -1,28 +1,58 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject} from '@nestjs/common';
 import neo4j from 'neo4j-driver';
 
 @Injectable()
 export class Neo4jService {
 	constructor(@Inject("Neo4j") private readonly neo4j) {}
-	async findAll(): Promise<any> {
-		const filter = "Containers encapsulate"
+
+	async findAll(filter): Promise<any> {
+
 		const session = this.neo4j.session()
 		try {
 		const result = await session.run(
 		  `CALL db.index.fulltext.queryNodes('gl_fulltext_node_index', $filter) YIELD node, score 
 			WITH node, score 
 			MATCH (parent)-[:parent]->(node)-[:parent]->(children)
-			WITH properties(node) as node, collect(properties(parent)) as parents, collect(properties(children)) as children, score 
-			RETURN {node: node, parents: parents, children: children} as json ORDER BY score DESC LIMIT 1`,
+			WITH properties(node) as node, node.IRI as node_id, collect(properties(parent)) as parents, collect(parent.IRI) as parents_ids, collect(properties(children)) as children, score 
+			RETURN {node: node, node_id: node_id, parents: parents, parents_ids: parents_ids, children: children} as json ORDER BY score DESC LIMIT 5`,
 		  {
 		    filter: filter 
 		  }
 		)
 		const records = result.records
 		const answer = []
+		// TODO: better create more elegant way to return format data needed 
 		for (let i = 0; i < records.length; i++) {
-		  const record = records[i]._fields[0]
-		  answer.push(record)
+			let nodes = []
+			const record = records[i]._fields[0]
+			// handle node (change IRI to id add parent_ids as parents)
+			let node = record.node
+			node.id = node.IRI
+			node.parents = record.parents_ids
+			delete node.IRI
+			// push node to nodes
+			nodes.push(node)
+
+			// handle parents (change IRI to id)			
+			let parents = record.parents.map((parent) => {
+			    parent.id = parent.IRI
+			    parent.parents = null
+			    delete parent.IRI
+			    return parent
+			})
+			// push parents to nodes
+			nodes = [...nodes, ...parents]
+			
+			// handle children (change IRI to id add node_id as parents)
+			let children = record.children.map((child) => {
+			    child.id = child.IRI
+			    child.parents = [node.id]
+			    delete child.IRI
+			    return child
+			})
+			// push children to nodes
+			nodes = [...nodes, ...children]
+			answer.push(nodes)
 		}
 		return answer
 		} finally {
