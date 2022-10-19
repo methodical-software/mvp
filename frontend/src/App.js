124cb190f00c65ecb6470c0b5ac38527ad932fd9
Konstantin <dev.shevchenko.konstantin@gmail.com>
2022-10-19 18:42:41 +0300

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import axios from "axios";
 import "./App.css";
-import { useLazyQuery, gql } from "@apollo/client";
 import { useState } from "react";
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -9,35 +9,57 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const GET_DOMAINS = gql`
-  query domain($filter: String){
-    characterSearch(filter: $filter){
-      IRI,
-      _updated,
-      description,
-      isDefinedBy,
-      label,
-      level,
-      type,
-    }
-  }
-`;
-
 function App() {
 
-  const [searchFilter, setSearchFilter] = useState('*');
-  const [executeSearch, { data }] = useLazyQuery(
-    GET_DOMAINS
-  );
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+
+  const handleClick = async () => {
+      setIsLoading(true);
+      const baseURL = 'http://localhost:3200/neo4j/'
+      const config = {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          "Accept": "application/json, text/plain, */*"
+        }
+      };
+      try {
+
+        const response = await axios.get(baseURL, config).then((res) => {
+
+          setData(res.data);
+        })
+        .catch((err) => {
+          console.log("AXIOS ERROR: ", err);
+        });
+
+        
+      } catch (err) {
+        setErr(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+  console.log(data);
+  const employees = [
+    {id: 1, name: 'Alice', tasks: ['dev', 'test', 'ship']},
+    {id: 2, name: 'Bob', tasks: ['design', 'test']},
+    {id: 3, name: 'Carl', tasks: ['test']},
+  ];
 
   return (
-    <div className="App">
+
+      <div className="App">
       <h1><a href="#nodes" aria-hidden="true" className="aal_anchor" id="nodes"><svg aria-hidden="true" className="aal_svg" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fillRule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>nodes</h1>
 
       <div>
         <label>Search</label>
         <input
-          onChange={(e) => setSearchFilter(e.target.value)}
+          // onChange={(e) => setSearchFilter(e.target.value)}
           type="string"
         />
       </div>
@@ -46,9 +68,10 @@ function App() {
 
       <button
         onClick={() =>{
-              executeSearch({
-                variables: { filter: searchFilter }
-              })
+          handleClick()
+              // executeSearch({
+              //   variables: { filter: searchFilter }
+              // })
             }
           }
       >
@@ -57,36 +80,85 @@ function App() {
 
       <br/>
 
-      {data ? data.characterSearch.map(({__typename , IRI, _updated, description,  isDefinedBy, label, level}) => (
-        <div align="center">
-        <Box sx={{ maxWidth: 275, display: 'flex',
-          flexWrap: 'wrap',
-          alignContent: 'center'  }} >
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              {IRI}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {description}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              {isDefinedBy}
-            </Typography>
-            <Typography variant="body2">
-              {label}
-            </Typography>
-            <Typography variant="body2">
-              {level}
-            </Typography>
-          </CardContent>
-        </Box>
+    <div>
+      {data ? data.map(({node, parents, children}, index) => (
+        <div key={index}>
+          
+            <div align="left">
+              {parents.map(({IRI}, index) => (
+                <div key={index}>
+                  <h2>Parent IRI: {IRI}</h2>
+                </div>
+              ))}
+            </div>
+
+            <div align="center">
+              <h2>Node: {node.IRI}</h2>
+            </div>
+
+            <div align="right">
+              {children.map(({IRI}, index) => (
+                <div key={index}>
+                  <h2>Children IRI: {IRI}</h2>
+                </div>
+              ))}
+            </div>
+        <hr/> 
         </div>
-      )): "loading..."}
+
+      )): "no data"}
+      </div>
+        </div>
+  );
+
+
+  // return (
+
+
+  //     {data ? data.map(({node, parents, children}) => (
+  //       <div align="left">
+  //       {parents.map(({__typename , IRI, _updated, description,  isDefinedBy, label, level}) => (
+        
+  //       <Box sx={{ maxWidth: 275, display: 'flex',
+  //         flexWrap: 'wrap',
+  //         alignContent: 'center'  }} >
+  //         <CardContent>
+  //           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+  //             {IRI}
+  //           </Typography>
+  //         </CardContent>
+  //       </Box>
+        
+  //       ))}
+  //       </div>
+  //       <>
+  //       <div align="center">
+  //       <Box sx={{ maxWidth: 275, display: 'flex',
+  //         flexWrap: 'wrap',
+  //         alignContent: 'center'  }} >
+  //         <CardContent>
+  //           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+  //             {node.IRI}
+  //           </Typography>
+  //           <Typography variant="h5" component="div">
+  //             {node.description}
+  //           </Typography>
+  //           <Typography sx={{ mb: 1.5 }} color="text.secondary">
+  //             {node.isDefinedBy}
+  //           </Typography>
+  //           <Typography variant="body2">
+  //             {node.label}
+  //           </Typography>
+  //           <Typography variant="body2">
+  //             {node.level}
+  //           </Typography>
+  //         </CardContent>
+  //       </Box>
+  //       </div>
+  //     )): "loading..."}
 
       
-    </div>
-
-  );
+  
 }
 
 export default App;
