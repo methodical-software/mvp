af2a83f3c64e7872a3b78520b2812779d3adc62c
Konstantin <dev.shevchenko.konstantin@gmail.com>
2022-10-26 14:30:50 +0300

    filters and button beauty

--- frontend-lib/src/App.js
+++ frontend-lib/src/App.js
@@ -7,14 +7,56 @@ import { GroupByType, PageFitMode, Enabled, Colors } from 'basicprimitives'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faComment, faCog } from '@fortawesome/free-solid-svg-icons'
 import TextField from '@mui/material/TextField'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MailIcon from '@mui/icons-material/Mail'
+import blackcert from './components/icons/black/certification.png'
+import whitecert from './components/icons/white/certification.png'
+import blackdomain from './components/icons/black/domain.png'
+import whitedomain from './components/icons/white/domain.png'
+import blackfrmwrkortool from './components/icons/black/framework_or_tool.png'
+import whitefrmwrkortool from './components/icons/white/framework_or_tool.png'
+import blackmetric from './components/icons/black/metric.png'
+import whitemetric from './components/icons/white/metric.png'
+import blackpattern from './components/icons/black/pattern.png'
+import whitepattern from './components/icons/white/pattern.png'
+import blackperson from './components/icons/black/person.png'
+import whiteperson from './components/icons/white/person.png'
+import blackperspective from './components/icons/black/perspective.png'
+import whiteperspective from './components/icons/white/perspective.png'
+import blackproject from './components/icons/black/project.png'
+import whiteproject from './components/icons/white/project.png'
+import blackroledescription from './components/icons/black/role_description.png'
+import whiteroledescription from './components/icons/white/role_description.png'
+import blacksynonym from './components/icons/black/synonym.png'
+import whitesynonym from './components/icons/white/synonym.png'
+import blackteam from './components/icons/black/team.png'
+import whiteteam from './components/icons/white/team.png'
+import blackunnode from './components/icons/black/unknown_node.png'
+import whiteunnode from './components/icons/white/unknown_node.png'
 
 function App() {
 
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState('');
-  const [searchFilter, setSearchFilter] = useState('');
-  
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [err, setErr] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
+  const [activeCert,setActiveCert]=useState(false)
+  const [activeDomain,setActiveDomain]=useState(false)
+  const [activeFrmwrkOrTool,setActiveFrmwrkOrTool]=useState(false)
+  const [activeMetric,setActiveMetric]=useState(false)
+  const [activePattern,setActivePattern]=useState(false)
+  const [activePerson,setActivePerson]=useState(false)
+  const [activePerspective,setActivePerspective]=useState(false)
+  const [activeProject,setActiveProject]=useState(false)
+  const [activeRoleDescription,setActiveRoleDescription]=useState(false)
+  const [activeSynonym,setActiveSynonym]=useState(false)
+  const [activeTeam,setActiveTeam]=useState(false)
+  const [activeUnnode,setActiveUnnode]=useState(false)
 
   const handleClick = async (searchFilter) => {
       setIsLoading(true);
@@ -47,7 +89,7 @@ function App() {
       } finally {
         setIsLoading(false);
       }
-    };
+    }
 
   return (
 
@@ -66,15 +108,54 @@ function App() {
       </div>
       
       <br/>
-
-
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ justifyContent: "center" }}>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveCert(!activeCert) }}>
+              <img src={ activeCert? blackcert : whitecert } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveDomain(!activeDomain) }}>
+              <img src={ activeDomain? blackdomain : whitedomain } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveFrmwrkOrTool(!activeFrmwrkOrTool) }}>
+              <img src={ activeFrmwrkOrTool? blackfrmwrkortool : whitefrmwrkortool } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveMetric(!activeMetric) }}>
+              <img src={ activeMetric? blackmetric : whitemetric } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActivePattern(!activePattern) }}>
+              <img src={ activePattern? blackpattern : whitepattern } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActivePerson(!activePerson) }}>
+              <img src={ activePerson? blackperson : whiteperson } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActivePerspective(!activePerspective) }}>
+              <img src={ activePerspective? blackperspective : whiteperspective } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveProject(!activeProject) }}>
+              <img src={ activeProject? blackproject : whiteproject } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveRoleDescription(!activeRoleDescription) }}>
+              <img src={ activeRoleDescription? blackroledescription : whiteroledescription } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveSynonym(!activeSynonym) }}>
+              <img src={ activeSynonym? blacksynonym : whitesynonym } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveTeam(!activeTeam) }}>
+              <img src={ activeTeam? blackteam : whiteteam } height="20" width="20"/>
+            </IconButton>
+            <IconButton size="large" color="inherit" onClick={e => { setActiveUnnode(!activeUnnode) }}>
+              <img src={ activeUnnode? blackunnode : whiteunnode } height="20" width="20"/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <br/>
-      <button
-        onClick={() =>{ handleClick( searchFilter ) }
-          }
+      <Button variant="contained"
+        onClick={() =>{ handleClick( searchFilter ) }}
       >
         Submit!
-      </button>
+      </Button>
     <div align="center" className="placeholder">
       {data ? data.items.map((items, index) => (
         <FamDiagram  key={index} centerOnCursor={true} config={{
